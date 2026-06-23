01784762a5ee69099ede4261844dbafcdf5a987e
wanderlee <wanderleedev@gmail.com>
2026-06-23 10:00:22 -0500

    refactor: remove unused isDevMode import from main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import {
 } from '@angular/platform-browser';
 import { AppRoutingModule } from './app/app-routing.module';
 import { ROOT_REDUCERS, ALL_EFFECTS } from './app/store/app.state';
-import { isDevMode, importProvidersFrom } from '@angular/core';
+import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
