673881f0f08f559f092fe931c8586f0414c33c3f
wanderlee <wanderleedev@gmail.com>
2026-06-23 09:49:10 -0500

    refactor: simplify main.ts by removing NgRx store and effects setup

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,10 @@ import {
   bootstrapApplication,
 } from '@angular/platform-browser';
 import { AppRoutingModule } from './app/app-routing.module';
-import { StoreModule } from '@ngrx/store';
 import { ROOT_REDUCERS, ALL_EFFECTS } from './app/store/app.state';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { isDevMode, importProvidersFrom } from '@angular/core';
-import { EffectsModule } from '@ngrx/effects';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(
-      BrowserModule,
-      AppRoutingModule,
-      StoreModule.forRoot(ROOT_REDUCERS),
-      StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
-      EffectsModule.forRoot(ALL_EFFECTS),
-    ),
-    provideClientHydration(),
-  ],
+  providers: [importProvidersFrom(BrowserModule, AppRoutingModule), provideClientHydration()],
 }).catch(err => console.error(err));
