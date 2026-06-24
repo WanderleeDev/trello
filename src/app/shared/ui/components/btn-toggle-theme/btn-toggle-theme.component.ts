aeaa6dae886f502ab6671600a5cd2f5675aa9c14
wanderlee <wanderleedev@gmail.com>
2026-06-24 10:01:53 -0500

    fix: revert btn-toggle-theme to use moon/sun icon components

--- src/app/shared/ui/components/btn-toggle-theme/btn-toggle-theme.component.ts
+++ src/app/shared/ui/components/btn-toggle-theme/btn-toggle-theme.component.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ThemeToggleService } from 'ngx-theme-stack';
+import { MoonIconComponent } from '../../../icons/components/moon-icon.component';
+import { SunIconComponent } from '../../../icons/components/sun-icon.component';
 
 @Component({
   selector: 'app-btn-toggle-theme',
   templateUrl: './btn-toggle-theme.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [MoonIconComponent, SunIconComponent],
 })
 export class BtnToggleThemeComponent {
   protected readonly theme = inject(ThemeToggleService);
