087623f740f0db2a9e798be53d80877b495d9341
wanderlee <wanderleedev@gmail.com>
2026-06-25 01:23:47 -0500

    fix: overflow scroll en list container, responsive sidebar

--- src/app/pages/workspace/components/board/board.component.ts
+++ src/app/pages/workspace/components/board/board.component.ts
@@ -1,15 +1,13 @@
-import { Component, output, input } from '@angular/core';
-import { DragDropModule, CdkDragDrop } from '@angular/cdk/drag-drop';
+import { Component, output, input, signal } from '@angular/core';
 import { List } from '../../interfaces/board.model';
-import { BoardHeaderComponent } from '../board-header/board-header.component';
-import { GrabScrollDirective } from '../../../../shared/directives/grab-scroll.directive';
+import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
+import { AddListButtonComponent } from '../add-list-button/add-list-button.component';
 
 @Component({
-    selector: 'app-board',
-    templateUrl: './board.component.html',
-    styleUrls: ['./board.component.css'],
-    standalone: true,
-    imports: [BoardHeaderComponent, DragDropModule, GrabScrollDirective],
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css'],
+  imports: [CdkDropList, CdkDrag, AddListButtonComponent],
 })
 export class BoardComponent {
   readonly boardName = input<string>('My Board');
@@ -18,5 +16,22 @@ export class BoardComponent {
 
   onListDropped(event: CdkDragDrop<List[]>) {
     this.listDropped.emit(event);
+  }
+
+  numbers = signal([1, 2, 3]);
+  drop(event: CdkDragDrop<unknown>) {
+    moveItemInArray(this.numbers(), event.previousIndex, event.currentIndex);
+  }
+  /**
+   * Predicate function that only allows even numbers to be
+   * sorted into even indices and odd numbers at odd indices.
+   */
+  sortPredicate(index: number, item: CdkDrag<number>) {
+    return (index + 1) % 2 === item.data % 2;
+  }
+
+  protected addNewColumn(title: string) {
+    console.log(title);
+    this.numbers.update(prev => [...prev, prev.length + 1]);
   }
 }
